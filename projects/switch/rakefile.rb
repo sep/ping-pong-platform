c8e905903b4d8036718af689cb9caacb2b67cee5
Themen Danielson <tsdanielson@sep.com>
2013-10-25 16:07:32 -0400

    initial MSP430 build command

--- projects/switch/rakefile.rb
+++ projects/switch/rakefile.rb
@@ -2,3 +2,13 @@ PROJECT_CEEDLING_ROOT = "../vendor/ceedling"
 load "#{PROJECT_CEEDLING_ROOT}/lib/rakefile.rb"
 
 task :default => %w[ test:all ]
+
+desc "build MSP430 code"
+task :buildit do
+	system "msp430-gcc -mmcu=msp430g2553 -O3 -I include -I ../communication/include src/led_driver.c -o out.elf"
+end
+
+desc "launches mspdebug for launchpad"
+task :mspdebug do
+	system "mspdebug rf2500"
+end
